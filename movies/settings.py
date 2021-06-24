9c5f8c3898d60204d2cfff6a4531e29341f369f0
djmuted <jaero13@gmail.com>
2021-06-24 19:02:57 +0200

    Add production API URL

--- movies/settings.py
+++ movies/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%82#wgzh7ikuxdm0!n@5wvtdw*!x4axwpp#m4c@(io5f=t8bcx'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-%82#wgzh7ikuxdm0!n@5wvtdw*!x4axwpp#m4c@(io5f=t8bcx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEPLOYMENT_ENV", "") != "PROD"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("API_URL", "localhost")]
 
 
 # Application definition
